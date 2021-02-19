c946033adee3c06f72600fc5b36be64fb28c20ee
JG Carvalho <jg@jgcarv.com>
2021-02-19 17:51:43 -0300

    fix functions location

--- contracts/TellorGetters.sol
+++ contracts/TellorGetters.sol
@@ -456,62 +456,14 @@ contract TellorGetters is TellorStorage, TellorVariables, Utilities {
         view
         returns (uint256[5] memory _requestIds)
     {
-        return _getTopRequestIDs();
-    }
-
-    /**
-     * @dev Getter function for the top 5 requests with highest payouts.
-     * This function is used within the newBlock function
-     * @return _requestIds the top 5 requests ids based on tips or the last 5 requests ids mined
-     */
-    function _getTopRequestIDs()
-        internal
-        view
-        returns (uint256[5] memory _requestIds)
-    {
         uint256[5] memory _max;
         uint256[5] memory _index;
-        (_max, _index) = _getMax5(requestQ);
+        (_max, _index) = getMax5(requestQ);
         for (uint256 i = 0; i < 5; i++) {
             if (_max[i] != 0) {
                 _requestIds[i] = requestIdByRequestQIndex[_index[i]];
             } else {
                 _requestIds[i] = currentMiners[4 - i].value;
-            }
-        }
-    }
-
-    /**
-     * @dev This is an internal function called by updateOnDeck that gets the top 5 values
-     * @param data is an array [51] to determine the top 5 values from
-     * @return max the top 5 values and their index values in the data array
-     */
-    function _getMax5(uint256[51] memory data)
-        internal
-        pure
-        returns (uint256[5] memory max, uint256[5] memory maxIndex)
-    {
-        uint256 min5 = data[1];
-        uint256 minI = 0;
-        for (uint256 j = 0; j < 5; j++) {
-            max[j] = data[j + 1]; //max[0]=data[1]
-            maxIndex[j] = j + 1; //maxIndex[0]= 1
-            if (max[j] < min5) {
-                min5 = max[j];
-                minI = j;
-            }
-        }
-        for (uint256 i = 6; i < data.length; i++) {
-            if (data[i] > min5) {
-                max[minI] = data[i];
-                maxIndex[minI] = i;
-                min5 = data[i];
-                for (uint256 j = 0; j < 5; j++) {
-                    if (max[j] < min5) {
-                        min5 = max[j];
-                        minI = j;
-                    }
-                }
             }
         }
     }
