58bae974012e37c0895f99ee2af64d8e4a20dea0
JG Carvalho <jg@jgcarv.com>
2021-03-01 10:50:56 -0300

    small adjustments on the contracts

--- contracts/ITellor.sol
+++ contracts/ITellor.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.7.4;
 
-abstract contract ITellor {
+interface ITellor {
     event NewTellorAddress(address _newTellor);
     event NewDispute(
         uint256 indexed _disputeId,
@@ -66,52 +66,42 @@ abstract contract ITellor {
     ); //ERC20 Approval event
     event Transfer(address indexed _from, address indexed _to, uint256 _value); //ERC20 Transfer Event
 
-    function changeDeity(address _newDeity) external virtual;
+    function changeDeity(address _newDeity) external;
 
-    function changeTellorContract(address _tellorContract) external virtual;
+    function changeTellorContract(address _tellorContract) external;
 
     function allowance(address _user, address _spender)
         external
         view
-        virtual
         returns (uint256);
 
     function allowedToTrade(address _user, uint256 _amount)
         external
         view
-        virtual
         returns (bool);
 
-    function balanceOf(address _user) external view virtual returns (uint256);
+    function balanceOf(address _user) external view returns (uint256);
 
     function balanceOfAt(address _user, uint256 _blockNumber)
         external
         view
-        virtual
         returns (uint256);
 
     function didMine(bytes32 _challenge, address _miner)
         external
         view
-        virtual
         returns (bool);
 
     function didVote(uint256 _disputeId, address _address)
         external
         view
-        virtual
         returns (bool);
 
-    function getAddressVars(bytes32 _data)
-        external
-        view
-        virtual
-        returns (address);
+    function getAddressVars(bytes32 _data) external view returns (address);
 
     function getAllDisputeVars(uint256 _disputeId)
-        public
+        external
         view
-        virtual
         returns (
             bytes32,
             bool,
@@ -127,7 +117,6 @@ abstract contract ITellor {
     function getCurrentVariables()
         external
         view
-        virtual
         returns (
             bytes32,
             uint256,
@@ -140,96 +129,82 @@ abstract contract ITellor {
     function getDisputeIdByDisputeHash(bytes32 _hash)
         external
         view
-        virtual
         returns (uint256);
 
     function getDisputeUintVars(uint256 _disputeId, bytes32 _data)
         external
         view
-        virtual
         returns (uint256);
 
-    function getLastNewValue() external view virtual returns (uint256, bool);
+    function getLastNewValue() external view returns (uint256, bool);
 
     function getLastNewValueById(uint256 _requestId)
         external
         view
-        virtual
         returns (uint256, bool);
 
     function getMinedBlockNum(uint256 _requestId, uint256 _timestamp)
         external
         view
-        virtual
         returns (uint256);
 
     function getMinersByRequestIdAndTimestamp(
         uint256 _requestId,
         uint256 _timestamp
-    ) external view virtual returns (address[5] memory);
+    ) external view returns (address[5] memory);
 
     function getNewValueCountbyRequestId(uint256 _requestId)
         external
         view
-        virtual
         returns (uint256);
 
     function getRequestIdByRequestQIndex(uint256 _index)
         external
         view
-        virtual
         returns (uint256);
 
     function getRequestIdByTimestamp(uint256 _timestamp)
         external
         view
-        virtual
         returns (uint256);
 
     function getRequestIdByQueryHash(bytes32 _request)
         external
         view
-        virtual
         returns (uint256);
 
-    function getRequestQ() public view virtual returns (uint256[51] memory);
+    function getRequestQ() external view returns (uint256[51] memory);
 
     function getRequestUintVars(uint256 _requestId, bytes32 _data)
         external
         view
-        virtual
         returns (uint256);
 
     function getRequestVars(uint256 _requestId)
         external
         view
-        virtual
         returns (uint256, uint256);
 
     function getStakerInfo(address _staker)
         external
         view
-        virtual
         returns (uint256, uint256);
 
     function getSubmissionsByTimestamp(uint256 _requestId, uint256 _timestamp)
         external
         view
-        virtual
         returns (uint256[5] memory);
 
     function getTimestampbyRequestIDandIndex(uint256 _requestID, uint256 _index)
         external
         view
-        virtual
         returns (uint256);
 
-    function getUintVar(bytes32 _data) public view virtual returns (uint256);
+    function getUintVar(bytes32 _data) external view returns (uint256);
 
     function getVariablesOnDeck()
         external
         view
-        virtual
         returns (
             uint256,
             uint256,
@@ -239,73 +214,64 @@ abstract contract ITellor {
     function isInDispute(uint256 _requestId, uint256 _timestamp)
         external
         view
-        virtual
         returns (bool);
 
     function retrieveData(uint256 _requestId, uint256 _timestamp)
         external
         view
-        virtual
         returns (uint256);
 
-    function totalSupply() external view virtual returns (uint256);
+    function totalSupply() external view returns (uint256);
 
     function beginDispute(
         uint256 _requestId,
         uint256 _timestamp,
         uint256 _minerIndex
-    ) external virtual;
+    ) external;
 
-    function vote(uint256 _disputeId, bool _supportsDispute) external virtual;
+    function vote(uint256 _disputeId, bool _supportsDispute) external;
 
-    function tallyVotes(uint256 _disputeId) external virtual;
+    function tallyVotes(uint256 _disputeId) external;
 
-    function proposeFork(address _propNewTellorAddress) external virtual;
+    function proposeFork(address _propNewTellorAddress) external;
 
-    function addTip(uint256 _requestId, uint256 _tip) external virtual;
+    function addTip(uint256 _requestId, uint256 _tip) external;
 
     function submitMiningSolution(
         string calldata _nonce,
         uint256[5] calldata _requestId,
         uint256[5] calldata _value
-    ) external virtual;
+    ) external;
 
-    function proposeOwnership(address payable _pendingOwner) external virtual;
+    function proposeOwnership(address payable _pendingOwner) external;
 
-    function claimOwnership() external virtual;
+    function claimOwnership() external;
 
-    function depositStake() external virtual;
+    function depositStake() external;
 
-    function requestStakingWithdraw() external virtual;
+    function requestStakingWithdraw() external;
 
-    function withdrawStake() external virtual;
+    function withdrawStake() external;
 
-    function approve(address _spender, uint256 _amount)
-        external
-        virtual
-        returns (bool);
+    function approve(address _spender, uint256 _amount) external returns (bool);
 
-    function transfer(address _to, uint256 _amount)
-        external
-        virtual
-        returns (bool);
+    function transfer(address _to, uint256 _amount) external returns (bool);
 
     function transferFrom(
         address _from,
         address _to,
         uint256 _amount
-    ) external virtual returns (bool);
+    ) external returns (bool);
 
-    function name() external pure virtual returns (string memory);
+    function name() external pure returns (string memory);
 
-    function symbol() external pure virtual returns (string memory);
+    function symbol() external pure returns (string memory);
 
-    function decimals() external pure virtual returns (uint8);
+    function decimals() external pure returns (uint8);
 
     function getNewCurrentVariables()
         external
         view
-        virtual
         returns (
             bytes32 _challenge,
             uint256[5] memory _requestIds,
@@ -316,49 +282,44 @@ abstract contract ITellor {
     function getTopRequestIDs()
         external
         view
-        virtual
         returns (uint256[5] memory _requestIds);
 
     function getNewVariablesOnDeck()
         external
         view
-        virtual
         returns (uint256[5] memory idsOnDeck, uint256[5] memory tipsOnDeck);
 
-    function updateTellor(uint256 _disputeId) external virtual;
+    function updateTellor(uint256 _disputeId) external;
 
-    function unlockDisputeFee(uint256 _disputeId) external virtual;
+    function unlockDisputeFee(uint256 _disputeId) external;
 
     //Test Functions
-    function theLazyCoon(address _address, uint256 _amount) external virtual;
+    function theLazyCoon(address _address, uint256 _amount) external;
 
     function testSubmitMiningSolution(
         string calldata _nonce,
         uint256[5] calldata _requestId,
         uint256[5] calldata _value
-    ) external virtual;
+    ) external;
 
-    function manuallySetDifficulty(uint256 _diff) external virtual {}
+    function manuallySetDifficulty(uint256 _diff) external;
 
-    function migrate() external {}
+    function migrate() external;
 
     function getMax(uint256[51] memory data)
-        public
+        external
         view
-        virtual
         returns (uint256 max, uint256 maxIndex);
 
     function getMin(uint256[51] memory data)
-        public
+        external
         view
-        virtual
         returns (uint256 min, uint256 minIndex);
 
     function getMax5(uint256[51] memory data)
-        public
+        external
         view
-        virtual
         returns (uint256[5] memory max, uint256[5] memory maxIndex);
 
-    function changeTellorGetters(address _tGetters) external virtual;
+    function changeTellorGetters(address _tGetters) external;
 }
