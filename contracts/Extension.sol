69c555a0c39320bc3a8114743c3b6d39923a63e1
JG Carvalho <jg@jgcarv.com>
2021-03-22 11:53:21 -0300

    added basic handshake

--- contracts/Extension.sol
+++ contracts/Extension.sol
@@ -137,8 +137,7 @@ contract Extension is TellorGetters {
             if (stakes.currentStatus == 3) {
                 stakes.currentStatus = 4;
             }
-        }
-        else if (
+        } else if (
             uint256(_tally) >=
             ((self.uintVars[keccak256("total_supply")] * 5) / 100)
         ) {
@@ -172,26 +171,22 @@ contract Extension is TellorGetters {
                 )
             ];
         TellorStorage.Dispute storage disp = disputesById[lastID];
-        require(msg.sender == disp.proposedForkAddress, "function needs to be run by the new contract");
-        require (disp.disputeUintVars[_FORK_EXECUTED] == 0, "update Tellor has already been run");
+        // require(msg.sender == disp.proposedForkAddress, "function needs to be run by the new contract");
+        require(
+            disp.disputeUintVars[_FORK_EXECUTED] == 0,
+            "update Tellor has already been run"
+        );
         require(disp.disputeVotePassed == true, "vote needs to pass");
         require(
             block.timestamp - disp.disputeUintVars[_TALLY_DATE] > 1 days,
             "Time for voting for further disputes has not passed"
         );
-        (bool success, bytes memory data) = Extension(_oldTellor).delegatecall(
-            abi.encodeWithSignature("balanceOf(address)", addresses(_OWNER))
-        );
-        uint value;
-        assembly {
-            value := mload(add(data, 0x20))
-        }
-        require(balanceOf(addresses(_OWNER) == value, "balances should be the same");
+
         disp.disputeUintVars[_FORK_EXECUTED] = 1;
         addresses[_TELLOR_CONTRACT] = disp.proposedForkAddress;
     }
 
-    function callUpdateTellor(address _oldTellor, uint256 _disputeID) public{
+    function callUpdateTellor(address _oldTellor, uint256 _disputeID) public {
         Extension(_oldTellor).updateTellor(_disputeID);
     }
 
