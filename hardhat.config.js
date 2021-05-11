44862d35d476fc66d501fb3b8f5b03580a385832
brendaloya <l.loya.g@gmail.com>
2021-05-11 15:13:59 -0400

    commented in some of the hardhat.config for coveralls

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,11 @@
-// require("@nomiclabs/hardhat-truffle5");
-// require("hardhat-gas-reporter");
-// require('hardhat-contract-sizer');
-// require("solidity-coverage");
-// require("@nomiclabs/hardhat-ethers");
-// require("@nomiclabs/hardhat-etherscan");
-// require("dotenv").config();
-// //require("@nomiclabs/hardhat-web3");
+require("@nomiclabs/hardhat-truffle5");
+require("hardhat-gas-reporter");
+require('hardhat-contract-sizer');
+require("solidity-coverage");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+require("@nomiclabs/hardhat-web3");
 
 
 // //Run this commands to deploy tellor:
@@ -130,52 +130,52 @@
 //  });
 
 
-// /**
-//  * @type import('hardhat/config').HardhatUserConfig
-//  */
-// module.exports = {
-//  solidity: {
-//     version: "0.7.4",
-//     settings: {
-//       optimizer: {
-//         enabled: true,
-//         runs: 300
-//       }
-//     }
-//   },
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+ solidity: {
+    version: "0.7.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 300
+      }
+    }
+  },
 
-//   networks: {
-//     hardhat: {
-//       accounts: {
-//         mnemonic:
-//           "nick lucian brenda kevin sam fiscal patch fly damp ocean produce wish",
-//         count: 40,
-//       },
-//       allowUnlimitedContractSize: true,
-//     },
-//       rinkeby: {
-//         url: `${process.env.NODE_URL_RINKEBY}`,
-//         accounts: [process.env.PRIVATE_KEY],
-//         gas: 10000000 ,
-//         gasPrice: 4000000000
-//       },
-//       // mainnet: {
-//       //   url: `${process.env.NODE_URL_MAINNET}`,
-//       //   accounts: [process.env.PRIVATE_KEY],
-//       //   gas: 10000000 ,
-//       //   gasPrice: 8000000000
-//       // }  
-//   },
-//   etherscan: {
-//       // Your API key for Etherscan
-//       // Obtain one at https://etherscan.io/
-//       apiKey: process.env.ETHERSCAN
-//     },
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic:
+          "nick lucian brenda kevin sam fiscal patch fly damp ocean produce wish",
+        count: 40,
+      },
+      allowUnlimitedContractSize: true,
+    },
+      // rinkeby: {
+      //   url: `${process.env.NODE_URL_RINKEBY}`,
+      //   accounts: [process.env.PRIVATE_KEY],
+      //   gas: 10000000 ,
+      //   gasPrice: 4000000000
+      // },
+      // mainnet: {
+      //   url: `${process.env.NODE_URL_MAINNET}`,
+      //   accounts: [process.env.PRIVATE_KEY],
+      //   gas: 10000000 ,
+      //   gasPrice: 8000000000
+      // }  
+  },
+  etherscan: {
+      // Your API key for Etherscan
+      // Obtain one at https://etherscan.io/
+      apiKey: process.env.ETHERSCAN
+    },
  
-//     contractSizer: {
-//       alphaSort: true,
-//       runOnCompile: true,
-//       disambiguatePaths: false,
-//     },
+    contractSizer: {
+      alphaSort: true,
+      runOnCompile: true,
+      disambiguatePaths: false,
+    },
 
-// };
+};
